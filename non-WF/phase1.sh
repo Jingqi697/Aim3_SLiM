80f3eb03f8c70f8f32ac5bb0d1e60432c47cfa6e
jingqiliu <lasiwk697@outlook.com>
2026-02-06 13:48:13 -0500

    Update phase1.sh

--- non-WF/phase1.sh
+++ non-WF/phase1.sh
@@ -6,8 +6,8 @@
 #SBATCH --mem=24G
 #SBATCH -p standard
 #SBATCH --account=berglandlab
-#SBATCH --output=/scratch/cqh6wn/Aim3_SLiM/WF/1Phase/logs/phase1.%A_%a.out
-#SBATCH --error=/scratch/cqh6wn/Aim3_SLiM/WF/1Phase/logs/phase1.%A_%a.err
+#SBATCH --output=/scratch/cqh6wn/Aim3_SLiM/nonWF/1Phase/logs/phase1.%A_%a.out
+#SBATCH --error=/scratch/cqh6wn/Aim3_SLiM/nonWF/1Phase/logs/phase1.%A_%a.err
 
 module purge
 module load slim
