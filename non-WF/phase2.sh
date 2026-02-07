7778dd77dc22337ff6ecebc0a0ae0561bd5e729c
jingqiliu <lasiwk697@outlook.com>
2026-02-06 21:03:01 -0500

    Create phase2.sh

---
+++ non-WF/phase2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J slim_phase2_inversion
+#SBATCH --ntasks-per-node=1
+#SBATCH -N 1
+#SBATCH -t 12:00:00
+#SBATCH --mem=24G
+#SBATCH -p standard
+#SBATCH --account=berglandlab_standard
+#SBATCH --output=/scratch/cqh6wn/Aim3_SLiM/nonWF/2Phase/logs/phase2.%A_%a.out
+#SBATCH --error=/scratch/cqh6wn//Aim3_SLiM/nonWF/2Phase/logs/phase2.%A_%a.err
+#SBATCH --array=1-1000
+
+module purge
+module load slim
+
+slim -d jobID=$SLURM_ARRAY_TASK_ID phase2_inversion.slim
